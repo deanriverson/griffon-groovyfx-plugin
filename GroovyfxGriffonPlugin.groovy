d6624555f26d48bda6241cbf27902125b08e304d
Dean Iverson <deanriverson@gmail.com>
2011-08-19 20:28:33 -0600

    Latest updates after first release.  Added LICENSE.txt file.

--- GroovyfxGriffonPlugin.groovy
+++ GroovyfxGriffonPlugin.groovy
@@ -1,23 +1,31 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Dean Iverson
+ */
 class GroovyfxGriffonPlugin {
-    // the plugin version
     def version = "0.1"
-    // the version or versions of Griffon the plugin is designed for
-    def griffonVersion = '0.9.3-beta-1 > *' 
-    // the other plugins this plugin depends on
+    def griffonVersion = '0.9.3 > *' 
     def dependsOn = [:]
-    // resources that are included in plugin packaging
     def pluginIncludes = []
-    // the plugin license
-    def license = '<UNKNOWN>'
-    // Toolkit compatibility. No value means compatible with all
-    // Valid values are: swing, javafx, swt, pivot, gtk
+    def license = 'Apache Software License 2.0'
     def toolkits = ['javafx']
-    // Platform compatibility. No value means compatible with all
-    // Valid values are:
-    // linux, linux64, windows, windows64, macosx, macosx64, solaris
     def platforms = []
 
-    // TODO Fill in these fields
     def author = 'Dean Iverson'
     def authorEmail = 'deanriverson@gmail.com'
     def title = 'Griffon GroovyFX Plugin'

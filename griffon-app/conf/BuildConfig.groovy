c37c1f160e8f2f3af023f4527b861c4106e99d73
aalmiray <aalmiray@gmail.com>
2012-01-12 18:26:49 +0100

    Update to 0.9.5

--- griffon-app/conf/BuildConfig.groovy
+++ griffon-app/conf/BuildConfig.groovy
@@ -1,22 +1,13 @@
 griffon.project.dependency.resolution = {
-    // inherit Griffon' default dependencies
-    inherits("global") {
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    inherits("global")
+    log "warn"
     repositories {
         griffonPlugins()
         griffonHome()
         griffonCentral()
-        flatDir name: 'groovyfxPluginLib', dirs: 'lib'
-
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenLocal()
-        //mavenCentral()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+        // pluginDirPath is only available when installed
+        String basePath = pluginDirPath? "${pluginDirPath}/" : ''
+        flatDir name: 'groovyfxPluginLib', dirs: ["${basePath}lib"]
     }
     dependencies {
         compile 'groovyx.javafx:groovyfx:0.1-SNAPSHOT'
@@ -31,6 +22,16 @@ griffon {
     }
 }
 
-griffon.jars.destDir='target/addon'
+log4j = {
+    // Example of changing the log pattern for the default console
+    // appender:
+    appenders {
+        console name: 'stdout', layout: pattern(conversionPattern: '%d [%t] %-5p %c - %m%n')
+    }
 
-//griffon.jars.jarName='GroovyfxGriffonAddon.jar'
+    error 'org.codehaus.griffon',
+          'org.springframework',
+          'org.apache.karaf',
+          'groovyx.net'
+    warn  'griffon'
+}
--- scripts/_Uninstall.groovy
+++ scripts/_Uninstall.groovy
@@ -1,18 +1,27 @@
-//
-// This script is executed by Griffon when the plugin is uninstalled from project.
-// Use this script if you intend to do any additional clean-up on uninstall, but
-// beware of messing up SVN directories!
-//
+/*
+ * Copyright 2010-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-// check to see if we already have a GroovyFXGriffonAddon
-boolean builderIsSet
-builderConfig.each() { prefix, v ->
-    v.each { builder, views ->
-        builderIsSet = builderIsSet || 'groovyx.javafx.SceneGraphBuilder' == builder
-    }
-}
+/**
+ * @author Dean Iverson
+ */
 
-if (builderIsSet) {
-    println 'Removing GroovyFX Builder from Builder.groovy'
-    builderConfigFile.text = builderConfigFile.text - "root.'groovyx.javafx.SceneGraphBuilder'.view = '*'\n"
+// check to see if we already have a SceneGraphBuilder
+builderConfigFile = new File(basedir, 'griffon-app/conf/Builder.groovy')
+configText = """root.'groovyx.javafx.SceneGraphBuilder'.view = '*'"""
+if(builderConfigFile.text.contains(configText)) {
+    println 'Removing SceneGraphBuilder from Builder.groovy'
+    builderConfigFile.text -= configText
 }
